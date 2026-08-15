333eee39f7402a1e5564db4653fd0deb1b1fa7f3
Sayan Mondal <sayanmondal0305@gmail.com>
2026-08-15 20:24:50 +0530

    Configure GitHub Pages deployment with static export

--- components/main/encryption.tsx
+++ components/main/encryption.tsx
@@ -48,14 +48,14 @@ export const Encryption = () => {
       <div className="flex flex-col items-center justify-center translate-y-[-50px] absolute z-[20] w-auto h-auto">
         <div className="flex flex-col items-center group cursor-pointer w-auto h-auto">
           <Image
-            src="/lock-top.png"
+            src="lock-top.png"
             alt="Lock top"
             width={50}
             height={50}
             className="translate-y-5 transition-all duration-200 group-hover:translate-y-11"
           />
           <Image
-            src="/lock-main.png"
+            src="lock-main.png"
             alt="Lock main"
             width={70}
             height={70}
@@ -84,7 +84,7 @@ export const Encryption = () => {
           style={{ y: smoothY }}
           className="w-full h-auto"
         >
-          <source src="/videos/encryption-bg.webm" type="video/webm" />
+          <source src="videos/encryption-bg.webm" type="video/webm" />
         </motion.video>
       </div>
     </div>
--- components/main/hero.tsx
+++ components/main/hero.tsx
@@ -45,7 +45,7 @@ export const Hero = () => {
         style={{ y: smoothY, scale: smoothScale }}
         className="rotate-180 absolute top-[-340px] left-0 w-full h-full object-cover -z-20"
       >
-        <source src="/videos/blackhole.webm" type="video/webm" />
+        <source src="videos/blackhole.webm" type="video/webm" />
       </motion.video>
 
       <HeroContent />
